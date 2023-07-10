3422c5c3a61220faf284a1eb2e98f5dc84c5cf22
messier-amaral <messieramaral.dev@gmail.com>
2023-07-10 17:45:52 -0300

    change of default content

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Hi! I am Messier Amaral and is the my first website with Next.js!</h1>
+    </main>
+  )
+}
