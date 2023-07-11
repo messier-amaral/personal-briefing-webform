a720fb8c9ba35eae88e724c8a838fa6b0afdd4db
messier-amaral <messieramaral.dev@gmail.com>
2023-07-11 16:30:41 -0300

    styling the homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ import styles from './page.module.css';
 import { BiCodeAlt } from 'react-icons/bi';
 import { PiPencilCircleDuotone } from 'react-icons/pi';
 import { CgBrowser } from 'react-icons/cg';
-import { HiOutlineSpeakerphone, HiSpeakerphone } from 'react-icons/hi';
+import { HiOutlineSpeakerphone } from 'react-icons/hi';
 
 import Rocket from '../../public/rocket-purple.png';
 import DevTeam from '../../public/dev-team-image.svg'
@@ -21,14 +21,16 @@ export default function Home() {
             src={Rocket}
             alt='Purple Rocket Logo'
             width={45}
+            id={styles.LogoPersonal}
           />
           <p id={styles.TitleLogo}>Messier</p>
-          <p id={styles.SubtileLogo}>Amaral</p>
+          <p id={styles.SubtitleLogo}>Amaral</p>
         </div>
         <Image 
           src={DevTeam}
           alt='Dev Team Image'
           width={380}
+          id={styles.DevTeamImage}
         />
       </div>
 
@@ -43,36 +45,35 @@ export default function Home() {
           Com uma análise cuidadosa, entrarei em contato o mais rápido possível para transformar suas ideias em realidade!
         </p>
 
+        <h2 id={styles.TitleSection}>Serviços Oferecidos</h2>
         <section className={styles.ServicesContainer}>
-          <h2 id={styles.TitleSection}>Serviços Oferecidos</h2>
-          <div className={styles.ServiceLeft}>
 
-            <span className={styles.Service}>
-              <BiCodeAlt />
-              <p>Desenvolvedor Full-Stack</p>
-            </span>
-            <span className={styles.Service}>
-              <PiPencilCircleDuotone />
-              <p>Designer Gráfico</p>
-            </span>
+            <div>
+              <div className={styles.Service}>
+                <BiCodeAlt className={styles.Icons} />
+                <p className={styles.NameService}>Desenvolvedor Full-Stack</p>
+              </div>
+              <div className={styles.Service}>
+                <PiPencilCircleDuotone className={styles.Icons} />
+                <p className={styles.NameService}>Designer Gráfico</p>
+              </div>
+            </div>
 
-          </div>
 
-          <div className={styles.ServiceRight}>
-
-            <span className={styles.Service}>
-              <CgBrowser />
-              <p>Designer Web / UI & UX</p>
-            </span>
-            <span className={styles.Service}>
-              <HiSpeakerphone />
-              <p>Especialista em Markentig</p>
-            </span>
-
-          </div>
-          <Link href='/'>Ver todos os Serviços</Link>
+            <div id={styles.SecondGroup}>
+              <div className={styles.Service}>
+                <CgBrowser className={styles.Icons} />
+                <p className={styles.NameService}>Designer Web / UI &amp; UX</p>
+              </div>
+              <div className={styles.Service}>
+                <HiOutlineSpeakerphone className={styles.Icons} />
+                <p className={styles.NameService}>Especialista em Marketing</p>
+              </div>
+            </div>
         </section>
-        <Link href='/'>Iniciar Formulário</Link>
+        <Link href='/' id={styles.MoreServicesLink}>Ver todos os Serviços</Link>
+
+        <Link href='/step-one' id={styles.LinkButton}>Iniciar Formulário</Link>
       </main>
     </div>
   )
